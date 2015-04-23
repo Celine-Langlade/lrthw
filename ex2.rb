bafe8241e849a1bb032de4cac5143ac0c5f54399
Céline LANGLADE <coklicote@hotmail.fr>
2015-04-23 18:19:09 +0200

    Fini les exercices 1 à 14

---
+++ ex2.rb
@@ -0,0 +1,5 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by ruby.
+puts "I could have code like this" #and the coment after is ignored
+# You can also use a coment to "disable" or coment out a piece of code :
+puts "This will run."
---
+++ ex7.rb
@@ -0,0 +1,21 @@
+puts "Mary had a little lamb."
+puts "Its fleece was white as #{'snow'}."
+puts "And everywhere that Mary went."
+puts "." * 10 #Wat'd that do? # écrit 10 fois le '.'
+
+end1 = "C" #écrit C
+end2 = "h" #écrit h
+end3 = "e" #écrit e
+end4 = "e" #écrit e
+end5 = "s" #écrit s
+end6 = "e" #écrit e
+end7 = "B" #écrit B
+end8 = "u" #écrit u
+end9 = "r" #écrit r
+end10 = "g" #écrit g 
+end11 = "e" #écrit e
+end12 = "r" #écrit r
+
+#watch that print vs. puts on this line what's it do.
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
